c088f372d9b710d8827df82832dcdcac93580c51
Gabriela <gabriela.razo@painani.mx>
2022-12-19 17:00:45 -0600

    Adding styled components to Home

--- src/layouts/main/App.js
+++ src/layouts/main/App.js
@@ -1,21 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola</h1>
     </div>
   );
 }
